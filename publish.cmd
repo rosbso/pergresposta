3c8b9e7a8da538eaa17ca5b63c40f4ea32f18343
robson <robson.arrudaf@gmail.com>
2018-07-05 14:42:31 -0400

    primeiro commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild AspNetCore-EchoBot-With-State.sln -p:DeployOnBuild=true -p:PublishProfile=pergresposta-Web-Deploy.pubxml -p:Password=uyq08tE0EoSPK2vHaPYz4gYfiM6v7kNnnBdSWjyfN3vzfl64DH3oiNieZ8fq
+
